622d3d0bdb71ea26f6aa2e84716ca2fd13ab2dad
Jean Lucien RANDRIANANTENAINA <rjlucienaina@gmail.com>
2022-05-26 05:33:40 +0300

    🚀 Add delete view.

--- forum/urls.py
+++ forum/urls.py
@@ -5,6 +5,10 @@ from . import views
 app_name = "forum_app"
 urlpatterns = [
     path('index/', views.indexView.as_view(), name='index'),
-    path('create/', views.createPost.as_view(), name='create'),
-    path('read/<int:pk>/', views.readPost.as_view(), name='read')
+    path('create/', views.createPost.as_view(), name='create'),  # CREATE
+    path('read/<int:pk>/', views.readPost.as_view(), name='read'),  # READ
+    path('read/<int:pk>/update', views.updatePost.as_view(),
+         name='update'),  # UPDATE
+    path('read/<int:pk>/delete', views.deletePost.as_view(),
+         name='delete'),  # DELETE
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Posts, PostComment
 from .forms import postForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Home page View
 
@@ -16,15 +17,47 @@ class indexView(ListView):
 
 class readPost(DetailView):
     model = Posts
-    context_object_name = "posts"
-    template_name = "forum/read.html"
+    context_object_name = "post"
+    template_name = "forum/post_read.html"
 
 
+# CREATE Post view
 class createPost(LoginRequiredMixin, CreateView):
-    template_name = "forum/post_cu.html"
+    template_name = "forum/post_create.html"
     model = Posts
     form_class = postForm
 
     def form_valid(self, form):
         form.instance.authorPost = self.request.user
         return super().form_valid(form)
+
+# Update post View
+
+
+class updatePost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "forum/post_update.html"
+    model = Posts
+    form_class = postForm
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.authorPost:
+            return True
+        else:
+            return False
+
+
+# Delete Post View
+
+class deletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Posts
+    context_object_name = "post"
+    template_name = "forum/post_delete.html"
+    success_url = reverse_lazy("forum_app:index")
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.authorPost:
+            return True
+        else:
+            return False
